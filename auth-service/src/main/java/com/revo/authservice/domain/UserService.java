1d5a24e3fb0c726c5c4b9008098f379fada2df30
mrrevo55 <kwolny31@gmail.com>
2022-06-25 20:05:05 +0200

    Implement UserService.java

--- auth-service/src/main/java/com/revo/authservice/domain/UserService.java
+++ auth-service/src/main/java/com/revo/authservice/domain/UserService.java
@@ -1,12 +1,15 @@
 package com.revo.authservice.domain;
 
 import com.revo.authservice.domain.dto.UserDto;
+import com.revo.authservice.domain.exception.BadLoginException;
 import com.revo.authservice.domain.exception.EmailInUseException;
 import com.revo.authservice.domain.exception.UsernameInUseException;
 import com.revo.authservice.domain.port.JwtPort;
 import com.revo.authservice.domain.port.UserRepositoryPort;
 import com.revo.authservice.domain.port.UserServicePort;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 public class UserService implements UserServicePort {
 
@@ -53,16 +56,39 @@ public class UserService implements UserServicePort {
 
     @Override
     public Mono<UserDto> getUserFromToken(String token) {
-        return null;
+        return userRepositoryPort
+                .getUserByUsername(getSubject(token));
+    }
+
+    private String getSubject(String token) {
+        return jwtPort.getSubject(token);
     }
 
     @Override
     public Mono<String> getTokenFromUsername(String username) {
-        return null;
+        return Mono
+                .just(createToken(username));
+    }
+
+    private String createToken(String username) {
+        return jwtPort.createToken(username);
     }
 
     @Override
     public Mono<UserDto> loginUser(UserDto userDto) {
-        return null;
+        return userRepositoryPort.getUserByUsername(userDto.username())
+                .flatMap(dto -> checkCredentials(dto, userDto));
     }
+
+    private Mono<UserDto> checkCredentials(UserDto baseDto, UserDto requestDto) {
+        if(passwordNotMatch(baseDto.password(), requestDto.password())){
+            return Mono.error(new BadLoginException());
+        }
+        return Mono.just(baseDto);
+    }
+
+    private boolean passwordNotMatch(String basePassword, String requestPassword) {
+        return !Objects.equals(basePassword, requestPassword);
+    }
+
 }
--- auth-service/src/main/java/com/revo/authservice/domain/port/UserRepositoryPort.java
+++ auth-service/src/main/java/com/revo/authservice/domain/port/UserRepositoryPort.java
@@ -7,5 +7,5 @@ public interface UserRepositoryPort {
     Mono<UserDto> save(UserDto userDto);
     Mono<Boolean> existsByEmail(String email);
     Mono<Boolean> existsByUsername(String username);
-    Mono<UserDto> getUserByUsername(String subject);
+    Mono<UserDto> getUserByUsername(String username);
 }
