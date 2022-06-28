d5ed913d75f436bb953b9888cbf3b5f755c7ba04
mrrevo55 <kwolny31@gmail.com>
2022-06-28 13:42:54 +0200

    Implemented tests in AuthControllerTest.java

--- auth-service/src/test/java/com/revo/authservice/infrastructure/application/rest/AuthControllerTest.java
+++ auth-service/src/test/java/com/revo/authservice/infrastructure/application/rest/AuthControllerTest.java
@@ -1,27 +1,32 @@
 package com.revo.authservice.infrastructure.application.rest;
 
-import com.revo.authservice.domain.UserService;
 import com.revo.authservice.domain.dto.UserDto;
 import com.revo.authservice.domain.exception.BadLoginException;
+import com.revo.authservice.domain.exception.UsernameInUseException;
 import com.revo.authservice.domain.port.UserServicePort;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @WebFluxTest(controllers = AuthController.class)
 @RunWith(SpringRunner.class)
 class AuthControllerTest {
+
+    private static final String REGISTER_END_POINT = "/register";
+    private static final String LOGIN_END_POINT = "/login";
+    private static final String USERNAME_IN_USE = "USERNAMEINUSE";
+    private static final String AUTHORIZE_END_POINT = "/authorize";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String EXAMPLE_TOKEN = "token";
 
     @Autowired
     private WebTestClient webTestClient;
@@ -39,37 +44,79 @@ class AuthControllerTest {
         //then
         webTestClient
                 .post()
-                .uri("/login")
+                .uri(LOGIN_END_POINT)
                 .bodyValue(userDto)
                 .exchange()
                 .expectStatus().isOk();
     }
 
     @Test
-    void shouldReturn401WhileLoggingUser(){
+    void shouldReturn401WhileLoggingUser() {
         //given
         //when
         when(userServicePort.loginUser(any(UserDto.class))).thenReturn(Mono.error(new BadLoginException()));
         //then
         webTestClient
                 .post()
-                .uri("/login")
+                .uri(LOGIN_END_POINT)
                 .bodyValue(userDto)
                 .exchange()
                 .expectStatus().isUnauthorized();
     }
 
     @Test
-    void registerUser() {
+    void shouldReturn201WhileRegisteringUser() {
         //given
         //when
+        when(userServicePort.createUser(any(UserDto.class))).thenReturn(Mono.just(userDto));
         //then
+        webTestClient
+                .post()
+                .uri(REGISTER_END_POINT)
+                .bodyValue(userDto)
+                .exchange()
+                .expectStatus().isCreated();
     }
 
     @Test
-    void translateTokenOnUser() {
+    void shouldReturn400WhileRegisteringUser() {
         //given
         //when
+        when(userServicePort.createUser(any(UserDto.class))).thenReturn(Mono.error(new UsernameInUseException(USERNAME_IN_USE)));
         //then
+        webTestClient
+                .post()
+                .uri(REGISTER_END_POINT)
+                .bodyValue(userDto)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void shouldReturn200WhileTranslatingTokenOnUser() {
+        //given
+        //when
+        when(userServicePort.getUserFromToken(anyString())).thenReturn(Mono.just(userDto));
+        //then
+        webTestClient
+                .post()
+                .uri(AUTHORIZE_END_POINT)
+                .header(AUTHORIZATION_HEADER, EXAMPLE_TOKEN)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void shouldReturn401WhileTranslatingTokenOnUser() {
+        //given
+        //when
+        when(userServicePort.getUserFromToken(anyString())).thenReturn(Mono.error(new BadLoginException()));
+        //then
+        webTestClient
+                .post()
+                .uri(AUTHORIZE_END_POINT)
+                .header(AUTHORIZATION_HEADER, EXAMPLE_TOKEN)
+                .exchange()
+                .expectStatus().isUnauthorized();
     }
 }
