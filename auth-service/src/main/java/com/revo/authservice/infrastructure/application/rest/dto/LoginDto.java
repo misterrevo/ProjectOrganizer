5d7b839b19e3493d45a23c1f884b2b550da39930
mrrevo55 <kwolny31@gmail.com>
2022-07-02 18:28:33 +0200

    Changed LoginDto.java and RegisterDto.java to records

--- auth-service/src/main/java/com/revo/authservice/infrastructure/application/rest/dto/LoginDto.java
+++ auth-service/src/main/java/com/revo/authservice/infrastructure/application/rest/dto/LoginDto.java
@@ -1,20 +1,3 @@
 package com.revo.authservice.infrastructure.application.rest.dto;
 
-public class LoginDto {
-
-    private String username;
-    private String password;
-
-    public LoginDto(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-}
+public record LoginDto (String username, String password){ }
--- auth-service/src/main/java/com/revo/authservice/infrastructure/application/rest/dto/RegisterDto.java
+++ auth-service/src/main/java/com/revo/authservice/infrastructure/application/rest/dto/RegisterDto.java
@@ -1,26 +1,3 @@
 package com.revo.authservice.infrastructure.application.rest.dto;
 
-public class RegisterDto {
-
-    private String username;
-    private String password;
-    private String email;
-
-    public RegisterDto(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-}
+public record RegisterDto(String username, String password, String email) { }
