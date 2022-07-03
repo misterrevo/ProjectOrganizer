36e86b4c62e7e9bc186e48812c769aadb7f72db5
mrrevo55 <kwolny31@gmail.com>
2022-07-03 21:55:59 +0200

    ...

--- auth-service/src/main/java/com/revo/authservice/infrastructure/application/rest/AuthController.java
+++ auth-service/src/main/java/com/revo/authservice/infrastructure/application/rest/AuthController.java
@@ -7,6 +7,7 @@ import com.revo.authservice.infrastructure.application.rest.dto.LoginDto;
 import com.revo.authservice.infrastructure.application.rest.dto.RegisterDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
