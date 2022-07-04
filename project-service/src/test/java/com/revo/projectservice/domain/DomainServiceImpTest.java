065b615cdfdb109e18880a4949c6e38e9c854a5d
mrrevo55 <kwolny31@gmail.com>
2022-07-04 14:02:55 +0200

    Start implementing DomainServiceImpTest.java

---
+++ project-service/src/test/java/com/revo/projectservice/domain/DomainServiceImpTest.java
@@ -0,0 +1,66 @@
+package com.revo.projectservice.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DomainServiceImpTest {
+
+    //TODO: Write tests
+
+    @Test
+    void getAllProjectsByToken() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void getProjectByTokenAndId() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void createProjectByToken() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void deleteProjectByTokenAndId() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void editProjectByTokenAndId() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void createTaskByTokenAndProjectId() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void editTaskByTokenAndId() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void deleteTaskByTokenAndId() {
+        //given
+        //when
+        //then
+    }
+}
